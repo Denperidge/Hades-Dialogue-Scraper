d8d5b239c60c6173942992acfd94548d9547710a
Denperidge <denperidge@gmail.com>
2022-11-18 00:40:43 +0100

    Adding encoding UTF-8 to export

--- app/export.py
+++ app/export.py
@@ -12,14 +12,14 @@ def export_dialogues(dialogues, output_dir, output_format="csv"):
         to_string = to_string_csv
 
 
-    with open(filename_all, "w") as file:
+    with open(filename_all, "w", encoding="UTF-8") as file:
         file.write(to_string(dialogues))
     
     for speaker in speakers:
         filename_speaker = join(output_dir, "{0}.{1}".format(speaker, output_format))
         # Same her as above. I just wanted to practice list comprehension. I'm so sorry
         dialogues_speaker = [dialogue for dialogue in dialogues if speaker in [sentence.speaker for sentence in dialogue.sentences]]
-        with open(filename_speaker, "w") as file:
+        with open(filename_speaker, "w", encoding="UTF-8") as file:
             file.write(to_string(dialogues_speaker))
 
 def to_string_csv(dialogues):
