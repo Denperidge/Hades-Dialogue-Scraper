4d431f16fff06dac8ef6ab80703397ccb1ef5a9a
Denperidge <denperidge@gmail.com>
2022-11-17 21:47:53 +0100

    Specified extract terms in docstring

--- app/extract.py
+++ app/extract.py
@@ -3,11 +3,15 @@ from glob import glob
 from re import match
 
 """
-Okay, so things are gonna get messy here. So lets define a few terms:
-- Dialogue: any spoken word. This 
+Okay, so things are gonna get messy here. So lets define a few terms as used in this code:
+- Conversation: when any amount of characters say anything.
+- Line: this will refer to a line in a FILE, not conversation.
+- Sentence: this will refer to a sentence/line in a CONVERSATION.
+- Dialogue: all the sentences said in a conversation/interaction.
+- (File) dialogues: a collection of multiple dialogues
 """
 
-def get_file_dialogues_from_folder(path):
+def get_dialogues_from_folder(path):
     files = glob(join(path, "*"))
     file_dialogues = []
     for file in files:
@@ -17,6 +21,8 @@ def get_file_dialogues_from_folder(path):
 def get_dialogues_from_file(path):
     with open(path, "r", encoding="UTF-8") as file:
         lines = file.readlines()
+
+    dialogues = []
     
     for line in lines:
         # If Event descriptor
