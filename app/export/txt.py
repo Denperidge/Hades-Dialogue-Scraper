73c860faa531abac8e968a3ed6bee6a4edcd1f39
Denperidge <denperidge@gmail.com>
2022-11-18 14:53:51 +0100

    Fixed .txt formatting, added .txt output

--- app/export/txt.py
+++ app/export/txt.py
@@ -3,12 +3,12 @@ from export.replace import parse_formatting_in_sentence
 def to_string_txt(dialogues):
     string = ""
     for dialogue in dialogues:
-        string += "---{}---\n".format(dialogue.description)
+        string += "--- {} ---\n".format(dialogue.description)
 
         for sentence in dialogue.sentences:
             sentence = parse_formatting_in_sentence(sentence, "*", "*")
 
-            string += "{0.speaker}|{0.text}\n".format(sentence)
+            string += str(sentence) + "\n"
         
         string += "\n\n"
     
